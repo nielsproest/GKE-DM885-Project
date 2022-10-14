eaca8af40e61f18be5a75c843564ff48f97b47c0
frederikgram <frederikxyz@hotmail.com>
2022-10-14 11:41:06 +0200

    missing final newline

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,9 +1,9 @@
-""" Meta tests for GitHub Actions workflows """
+"""Meta tests for GitHub Actions workflows"""
 
 import pytest
 
 @pytest.mark.pytest
 def test_pytest():
-    """ Test that pytest is working """
+    """Test that pytest is working"""
 
     assert True
