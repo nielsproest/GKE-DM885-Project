c048137c9c3277e6f90f10dec5a2afd0d22144fe
frederikgram <frederikxyz@hotmail.com>
2022-10-15 21:56:49 +0200

    Fix pylint score

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,10 +1,12 @@
+""" File for testing meta functionality such as PyLint for GitHub actions workflows  """
 
 import pytest
 
 @pytest.mark.pytest
 def test_pytest():
+    """ This output doesn't matter, it's just a method
+    to check if the pylinter scores the code 6.5 or above.
+    """
 
-    assert True   
-
-
+    assert True
 
