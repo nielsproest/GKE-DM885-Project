8b38a02f060d750e2255c3dfb15cfc2e11de3c10
Troels Andersen <trand19@student.sdu.dk>
2022-11-25 14:08:29 +0100

    Change config for terraform

--- terraform/gar.tf
+++ terraform/gar.tf
@@ -1,8 +1,9 @@
 
 
 resource "google_artifact_registry_repository" "devops_repo" {
+  project       = "solveit-369711"
   location      = "europe-west4"
-  repository_id = "services_repository"
+  repository_id = "services-repository"
   description   = "Repository for service images"
   format        = "DOCKER"
   provider      = google-beta
