a2a88e1fbddef4fb62dec8f09c7b9d802db1e234
Anders Black Larsen <ablack@pop-os.localdomain>
2022-12-07 21:56:35 +0100

    solver_service, login_service and job_service implmentations

--- frontend/www/scripts/settings_scripts.js
+++ frontend/www/scripts/settings_scripts.js
@@ -1,7 +1,39 @@
+const solverUrl = "http://127.0.0.1:8000"
+
 function loadChecker(){
   // Can the user be here? (is admin)
 
   // Load admin details
+  loadSolvers()
+}
+
+function loadSolvers(){
+
+  if(solverUrl != null){
+    fetch(solverUrl + "/solver", {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin':'*'
+      }
+    })
+      .then((response) => response.json())
+      .then((result) => {
+
+        solverList = document.getElementById("solverList")
+        solverList.innerHTML = "";
+        let solverParser = new DOMParser();
+        
+        result.forEach(solver => {
+          let solverToAppend = solverParser.parseFromString('<li class="list-group-item d-flex w-100 justify-content-between">' + solver.name + '<button id="' + solver.id + '" type="button" class="btn btn-outline-danger btn-sm" onclick="deleteSolver(this.id)">Delete solver</button></li>', 'text/html')
+          solverList.append(solverToAppend.documentElement)
+        })
+        
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
 }
 
 function makeUserAdmin() {
@@ -26,6 +58,51 @@ exampleModal.addEventListener('show.bs.modal', event => {
 
 
 function uploadNewSolver() {
+
+  solverName = document.getElementById("solver_id_input").value;
+  newsolverUrl = document.getElementById("solver_url_input").value;  
+
+  if(solverUrl != null){
+    fetch(solverUrl + "/solver/" + solverName + "/" + newsolverUrl, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin':'*'
+      }
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        
+        loadSolvers()
+        
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+}
+
+function deleteSolver(solverId) {
+
+  if(solverUrl != null){
+    fetch(solverUrl + "/solver/{id}?solverId=" + solverId, {
+      method: 'DELETE',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin':'*'
+      }
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        
+        loadSolvers()
+        
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
 
 }
 
