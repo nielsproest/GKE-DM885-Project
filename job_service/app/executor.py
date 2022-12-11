3eabfcd419951bd36c1dd8f118b9b3af5e714b64
Troels Andersen <trand19@student.sdu.dk>
2022-12-11 20:35:03 +0100

    Add concurrent handling of multiple jobs, where first one is selected

--- job_service/app/executor.py
+++ job_service/app/executor.py
@@ -1,9 +1,13 @@
 import logging
 import uuid
+import time
+import queue
 import os
+from threading import Thread
 
 from kubernetes import client
 from kubernetes import config
+from kubernetes import watch
 
 logging.basicConfig(level=logging.INFO)
 
@@ -77,7 +81,7 @@ class Kubernetes:
         pvc = client.V1PersistentVolumeClaimVolumeSource(claim_name=pvc_name)
 
 
-        #TODO: Make sure to sanitize solver name to avoid command injection!
+        #TODO: Make sure to sanitize solver name to avoid command injection?
         container_list = []
         for solver in solver_list:
           if has_dzn:
@@ -103,32 +107,82 @@ class Kubernetes:
 
     @staticmethod
     def create_pod_template(pod_name, containers):
-        volume = client.V1Volume(
-            name='job-pvc',
-            persistent_volume_claim=client.V1PersistentVolumeClaimVolumeSource(claim_name="job-pvc")
-        )
+        pod_list = []
+        for container in containers:
+            volume = client.V1Volume(
+                name='job-pvc',
+                persistent_volume_claim=client.V1PersistentVolumeClaimVolumeSource(claim_name="job-pvc")
+            )
 
-        pod_template = client.V1PodTemplateSpec(
-            spec=client.V1PodSpec(restart_policy="Never", containers=containers, volumes=[volume]),
-            metadata=client.V1ObjectMeta(name=pod_name, labels={"pod_name": pod_name}),
-        )
+            pod_template = client.V1PodTemplateSpec(
+                spec=client.V1PodSpec(restart_policy="Never", containers=[container], volumes=[volume]),
+                metadata=client.V1ObjectMeta(name=pod_name, labels={"pod_name": pod_name}),
+            )
+            pod_list.append(pod_template)
 
-        return pod_template
+
+        return pod_list
 
     @staticmethod
-    def create_job(job_name, pod_template):
-        metadata = client.V1ObjectMeta(name=job_name, labels={"job_name": job_name})
+    def create_job(pod_templates, job_id):
 
-        job = client.V1Job(
-            api_version="batch/v1",
-            kind="Job",
-            metadata=metadata,
-            spec=client.V1JobSpec(backoff_limit=0, template=pod_template, completions=1),
-        )
+        job_list = []
+        index = 0
+        for pod in pod_templates:
+            job_name = f"{pod.metadata.name}-{index}"
+            metadata = client.V1ObjectMeta(name=job_name, labels={"job": job_id})
 
-        return job
+            job = client.V1Job(
+                api_version="batch/v1",
+                kind="Job",
+                metadata=metadata,
+                spec=client.V1JobSpec(backoff_limit=0, template=pod, completions=1),
+            )
+            job_list.append(job)
+            index += 1
+
+        return job_list
+
+    def start_job(self, job, batch_api, _namespace, q, job_id):
+      start_time = time.time()
+      batch_api.create_namespaced_job(_namespace, job)
+      print(f"starting thread with job: {job.metadata.name}")
+
+      w = watch.Watch()
+      for event in w.stream(func=self.core_api.list_namespaced_pod,
+                                namespace=_namespace,
+                                label_selector=f"job-name={job.metadata.name}",
+                                timeout_seconds=20):
+          if event["object"].status.phase == "Succeeded":
+              pod_name = event['object'].metadata.name
+              w.stop()
+              end_time = time.time()
+              logging.info("Started in %0.2f sec", end_time-start_time)
+
+              result = self.core_api.read_namespaced_pod_log(name=pod_name, namespace=_namespace)
+              q.put((job.metadata.name, result))
+              return
+
+          # TODO: Clean up this code:
+          # event.type: ADDED, MODIFIED, DELETED
+          if event["type"] == "DELETED":
+              # Pod was deleted while we were waiting for it to start.
+              logging.info("Deleted before it started")
+              w.stop()
+              return
+
+      print("Reached here")
+      q.put(None)
+
+      return
 
 
+def thread_controller(q):
+  first = q.get()
+  print(f"First thread was: {first}")
+  # TODO: Stop other jobs
+  # TODO: Update database entry for job with result and new status
+  return
 
 def execute_job(create_job_request, mzn, dzn):
 
@@ -137,16 +191,11 @@ def execute_job(create_job_request, mzn, dzn):
 
     has_dzn = dzn != None
 
-    if os.getenv('KUBERNETES_SERVICE_HOST'):
-      mount_path = "/mnt"
-    else:
-      mount_path = "mnt"
-
-    with open(f"{mount_path}/{job_id}.mzn", "a") as f:
+    with open(f"/mnt/{job_id}.mzn", "a") as f:
       f.write(mzn)
-      if has_dzn:
-        with open(f"mount_path/{job_id}.dzn", "a") as f:
-          f.write(dzn)
+    if has_dzn:
+      with open(f"/mnt/{job_id}.dzn", "a") as f:
+        f.write(dzn)
 
     # Kubernetes instance
     k8s = Kubernetes()
@@ -159,14 +208,20 @@ def execute_job(create_job_request, mzn, dzn):
     solver_containers = k8s.create_container(create_job_request.solver_list, job_id, has_dzn, _pull_policy)
 
     _pod_name = f"solver-instance-pod-{pod_id}"
-    _pod_spec = k8s.create_pod_template(_pod_name, solver_containers)
+    _pod_specs = k8s.create_pod_template(_pod_name, solver_containers)
 
-    _job_name = f"solver-instance-job-{job_id}"
-    _job = k8s.create_job(_job_name, _pod_spec)
+    jobs = k8s.create_job(_pod_specs, job_id)
 
 
     batch_api = client.BatchV1Api()
-    batch_api.create_namespaced_job(_namespace, _job)
+    q = queue.Queue()
+    controller_thread = Thread(target = thread_controller, args = (q, ))
+    controller_thread.start()
+    for job in jobs:
+      thread = Thread(target = k8s.start_job, args = (job, batch_api, _namespace, q, job_id))
+      thread.start()
+
 
 if __name__ == "__main__":
   execute_job()
+
