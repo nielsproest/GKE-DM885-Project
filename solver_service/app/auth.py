fdde3c7860ef3cf4ebf443a9b1f0a3479cd36172
phonicmas <jkskovlund@gmail.com>
2022-12-17 13:29:39 +0100

    pk test

--- solver_service/app/auth.py
+++ solver_service/app/auth.py
@@ -1,7 +1,9 @@
 import jwt
 import time
 
-PUBLIC_KEY = ""
+PUBLIC_KEY = '''-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvpAXDxizoN4MHs0qJrQ9J/Dc+95mLbT7o/haw2vXuB2LoSp855W/5hpPqyhAkPmKJEzICp6Ke72a2oUVeJb8lckM3km9dxFBvNsbMEpKEOO1/WhmWw8aDwBI7E0s7KAXHSdqCBncB4L3W37O9c6bQ2QrGpfrN82yFXez25tdv1ODc7bzfYFdD5LHNVymYl0E+dR/4P2P/+YxUX7omUI9Bqt6jdw6uERt2tcyT0PFT2DQwf3mtrXCufo68uMfxKP0TN5c1Zan4jwXeiJE4wHPzFgaWTzgKB6xayJqkgI9nhy5KaONIKe+ZCerrsBKztk9R8uH38GdI2rcwCPYi2AkkQIDAQAB
+-----END PUBLIC KEY-----'''
 
 def setPublicKey(newKey):
     global PUBLIC_KEY
@@ -9,8 +11,10 @@ def setPublicKey(newKey):
 
 def validate_token(token: str) -> bool:
     try:
+        print(PUBLIC_KEY)
+        print(token)
         payload = decode_jwt(token)
-
+        print(payload)
         if time.time() > float(payload["expiration"]):
             return False
 
