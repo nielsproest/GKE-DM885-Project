3b3f8d3c5220b0c50588cf7c42ed1a10d657cd40
Niels <niroe18@student.sdu.dk>
2022-12-19 15:37:17 +0100

    FS changes

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,6 +1,9 @@
 import requests, pytest, httpx
 from config import *
 
+test_user = "test123"
+test_pass = "above_8_chars"
+
 @pytest.fixture
 def get_token():
 	with requests.post(auth_url + "/users/login", json={
@@ -9,3 +12,22 @@ def get_token():
 		}) as r:
 		return r.json()["token"]
 
+@pytest.fixture
+def test_create_user():
+	with httpx.Client() as client:
+		response = client.post(
+			auth_url + "/users/signup",
+			json={"username": test_user, "password": test_pass}
+		)
+		assert response.status_code == 200
+		assert response.json()["message"] == "OK"
+
+@pytest.fixture
+def test_create_user():
+	with httpx.Client() as client:
+		response = client.post(
+			auth_url + "/users/signup",
+			json={"username": test_user, "password": test_pass}
+		)
+		assert response.status_code == 200
+		assert response.json()["message"] == "OK"
